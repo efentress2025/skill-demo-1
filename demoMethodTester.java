0e5aa2ec8662bcd1fc14d18e6a3043b617e75fb5
notweezer123 <efentress@ucsd.edu>
2022-05-01 18:31:06 -0700

    Update Test

--- demoMethodTester.java
+++ demoMethodTester.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class demoMethodTester {
     @Test
     public void mult3ByTwo() {
-        assertEquals(7, demoMethod.multByTwo(3));
+        assertEquals(6, demoMethod.multByTwo(3));
     }
 }
